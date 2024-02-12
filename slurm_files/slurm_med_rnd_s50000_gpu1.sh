367f8e67ca182fd50d121a9efac08dc451dfc0bb
angelika1108 <angelika11085004@gmail.com>
2024-02-12 01:58:40 +0100

    run jean zay and config changes

--- slurm_files/slurm_med_rnd_s50000_gpu1.sh
+++ slurm_files/slurm_med_rnd_s50000_gpu1.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=latent_m_s50000_gpu1  # name of job
+#SBATCH --job-name=med_rnd_s50000_gpu1  # name of job
 #SBATCH -C v100-32g 							   # reserving 16 GB GPUs only if commented
 ##SBATCH --partition=gpu_p2                        # uncomment for gpu_p2 partition gpu_p2
 #SBATCH --ntasks=1					 			   # total number of processes (= number of GPUs here)
@@ -9,15 +9,15 @@
 #SBATCH --cpus-per-task=10           			   # number of cores per task (1/4 of the 4-GPUs node)
 # /!\ Caution, "multithread" in Slurm vocabulary refers to hyperthreading.
 #SBATCH --hint=nomultithread         			   # hyperthreading is deactivated
-#SBATCH --time=10:00:00             			   # maximum execution time requested (HH:MM:SS)
+#SBATCH --time=05:00:00             			   # maximum execution time requested (HH:MM:SS)
 ##SBATCH --time=00:10:00             			   # maximum execution time requested (HH:MM:SS)
-#SBATCH --output=slurm_logs/latent_m_s50000_gpu1_%j.output   # name of output file
-#SBATCH --error=slurm_logs/latent_m_s50000_gpu1_%j.error     # name of error file (here, in common with the output file)
+#SBATCH --output=slurm_logs/med_rnd_s50000_gpu1_%j.output   # name of output file
+#SBATCH --error=slurm_logs/med_rnd_s50000_gpu1_%j.error     # name of error file (here, in common with the output file)
 ##SBATCH --qos=qos_gpu-t4                          # for running (max 100h)
 #SBATCH --qos=qos_gpu-t3                          # for running (max 20h)
 ##SBATCH --qos=qos_gpu-dev                          # for veryfuing that the code is running.
 
-EXP_NAME="latent_m_s50000_gpu1"
+EXP_NAME="med_rnd_s50000_gpu1"
 OUTPUT_DUMP="${WORK}/experiments/latentpixelsplat/${EXP_NAME}"
 
 mkdir -p "${OUTPUT_DUMP}"
@@ -36,5 +36,5 @@ set -x
 # Code execution
 # python -m torch.distributed.launch --nproc_per_node=4 --use_env main.py \
 #   --output_dir "${OUTPUT_DUMP}" --data_path "/gpfsdsscratch/acid/" \
-python3 -m src.main +experiment=acid data_loader.train.batch_size=1 trainer.val_check_interval=30 optimizer.warm_up_steps=1000 checkpointing.load=pretrained_models/acid_latent_d3_f4_noattn.ckpt checkpointing.every_n_train_steps=2000 trainer.max_steps=50000
-  
+python3 -m src.main +experiment=acid data_loader.train.batch_size=1 trainer.val_check_interval=30 optimizer.warm_up_steps=1000 checkpointing.every_n_train_steps=2000 trainer.max_steps=50000
+
