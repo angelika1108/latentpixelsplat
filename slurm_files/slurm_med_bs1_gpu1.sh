7512240afbaf9125bb2c391621ad420a3b53bc29
angelika1108 <angelika11085004@gmail.com>
2024-02-20 00:16:07 +0100

    slurm

--- slurm_files/slurm_med_bs1_gpu1.sh
+++ slurm_files/slurm_med_bs1_gpu1.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=tiny_rnd_bs1_gpu1  # name of job
+#SBATCH --job-name=med_bs1_gpu1  # name of job
 #SBATCH -C v100-32g 							   # reserving 16 GB GPUs only if commented
 ##SBATCH --partition=gpu_p2                        # uncomment for gpu_p2 partition gpu_p2
 #SBATCH --ntasks=1					 			   # total number of processes (= number of GPUs here)
@@ -11,13 +11,13 @@
 #SBATCH --hint=nomultithread         			   # hyperthreading is deactivated
 #SBATCH --time=20:00:00             			   # maximum execution time requested (HH:MM:SS)
 ##SBATCH --time=00:10:00             			   # maximum execution time requested (HH:MM:SS)
-#SBATCH --output=slurm_logs/tiny_rnd_bs1_gpu1_%j.output   # name of output file
-#SBATCH --error=slurm_logs/tiny_rnd_bs1_gpu1_%j.error     # name of error file (here, in common with the output file)
+#SBATCH --output=slurm_logs/med_bs1_gpu1_%j.output   # name of output file
+#SBATCH --error=slurm_logs/med_bs1_gpu1_%j.error     # name of error file (here, in common with the output file)
 ##SBATCH --qos=qos_gpu-t4                          # for running (max 100h)
 #SBATCH --qos=qos_gpu-t3                          # for running (max 20h)
 ##SBATCH --qos=qos_gpu-dev                          # for veryfuing that the code is running.
 
-EXP_NAME="tiny_rnd_bs1_gpu1"
+EXP_NAME="med_bs1_gpu1"
 OUTPUT_DUMP="${WORK}/experiments/latentpixelsplat/${EXP_NAME}"
 
 mkdir -p "${OUTPUT_DUMP}"
@@ -36,5 +36,5 @@ set -x
 # Code execution
 # python -m torch.distributed.launch --nproc_per_node=4 --use_env main.py \
 #   --output_dir "${OUTPUT_DUMP}" --data_path "/gpfsdsscratch/acid/" \
-python3 -m src.main +experiment=acid data_loader.train.batch_size=1 wandb.mode=offline checkpointing.every_n_train_steps=10000
+python3 -m src.main +experiment=acid data_loader.train.batch_size=1 wandb.mode=offline checkpointing.load=pretrained_models/acid_latent_d3_f4_noattn.ckpt checkpointing.every_n_train_steps=10000
 
