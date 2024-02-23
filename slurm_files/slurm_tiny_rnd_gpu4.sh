79a28ee375d336ec1350fd80e2db9c46b9f3191f
angelika1108 <angelika11085004@gmail.com>
2024-02-23 23:22:48 +0100

    slurm

--- slurm_files/slurm_tiny_rnd_gpu4.sh
+++ slurm_files/slurm_tiny_rnd_gpu4.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=tiny_rnd_gpu4_debug  # name of job
+#SBATCH --job-name=tiny_rnd_gpu4  # name of job
 ##SBATCH -C v100-32g 							   # reserving 16 GB GPUs only if commented
 ##SBATCH --partition=gpu_p2                        # uncomment for gpu_p2 partition gpu_p2
 ##SBATCH --ntasks=4					 			   # total number of processes (= number of GPUs here)
@@ -9,15 +9,15 @@
 #SBATCH --cpus-per-task=10           			   # number of cores per task (1/4 of the 4-GPUs node)
 # /!\ Caution, "multithread" in Slurm vocabulary refers to hyperthreading.
 #SBATCH --hint=nomultithread         			   # hyperthreading is deactivated
-##SBATCH --time=20:00:00             			   # maximum execution time requested (HH:MM:SS)
-#SBATCH --time=00:10:00             			   # maximum execution time requested (HH:MM:SS)
-#SBATCH --output=slurm_logs/tiny_rnd_gpu4_debug_%j.output   # name of output file
-#SBATCH --error=slurm_logs/tiny_rnd_gpu4_debug_%j.error     # name of error file (here, in common with the output file)
-##SBATCH --qos=qos_gpu-t4                          # for running (max 100h)
+#SBATCH --time=99:00:00             			   # maximum execution time requested (HH:MM:SS)
+##SBATCH --time=00:10:00             			   # maximum execution time requested (HH:MM:SS)
+#SBATCH --output=slurm_logs/tiny_rnd_gpu4_%j.output   # name of output file
+#SBATCH --error=slurm_logs/tiny_rnd_gpu4_%j.error     # name of error file (here, in common with the output file)
+#SBATCH --qos=qos_gpu-t4                          # for running (max 100h)
 ##SBATCH --qos=qos_gpu-t3                          # for running (max 20h)
-#SBATCH --qos=qos_gpu-dev                          # for veryfuing that the code is running.
+##SBATCH --qos=qos_gpu-dev                          # for veryfuing that the code is running.
 
-EXP_NAME="tiny_rnd_gpu4_debug"
+EXP_NAME="tiny_rnd_gpu4"
 OUTPUT_DUMP="${WORK}/experiments/latentpixelsplat/${EXP_NAME}"
 
 mkdir -p "${OUTPUT_DUMP}"
